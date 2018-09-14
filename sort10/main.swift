75e6d9a0b0b4ba7a2e15add32e0e2b5f711c05b2
imnu2016 <1428373962@qq.com>
2018-09-14 17:12:10 +0800

    Initial Commit

---
+++ sort10/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sort10
+//
+//  Created by Hxxguohua on 2018/9/14.
+//  Copyright © 2018年 Hxxguohua. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
